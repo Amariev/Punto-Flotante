3d81106eaa8ae713d1a9fa6a1e29c13e5c8dcdc7
CamiaCL <camilaclary@gmail.com>
2022-04-26 08:31:19 -0500

    :art: tools
    wiiiiiii

---
+++ tools.cpp
@@ -0,0 +1,44 @@
+#include "tools.h"
+
+string cambioDeBaseEntero(int n){
+  string str;
+  while (n > 0) {
+    if(n%2) str.push_back('1');
+    else str.push_back('0');
+    n = n/2;
+  }
+  string temp(str.rbegin(), str.rend()); // revert
+  str = temp;
+  return str;
+}
+
+string cambioDeBaseDecimal(float f, const int size){
+  string str;
+  for (int i = 0; i < size; i++) {
+    f *= 2;
+    if ((int)f == 1) {
+      f--;
+      str.push_back('1');
+    }else str.push_back('0');
+  }
+  return str;
+}
+
+string cambioDeBaseDecimal(double f, const int size){
+  string str;
+  for (int i = 0; i < size; i++) {
+    f *= 2;
+    if ((int)f == 1) {
+      f--;
+      str.push_back('1');
+    }else str.push_back('0');
+  }
+  return str;
+}
+
+string cambioBiased(int n,const int size){
+  string str;
+  int num = (n-1) + ((pow(2, size-1))-1);
+  str = cambioDeBaseEntero(num);
+  return str;
+}
